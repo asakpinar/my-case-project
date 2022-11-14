ee37a2889d2cfe55719dc9c2df34eff32fe48b6c
Ahmet Şahin Akpınar <ahmetsahinakpnarr@gmail.com>
2022-11-14 22:18:18 +0300

    other

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatatableComponent } from './components/datatable/datatable.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { EditdialogComponent } from './components/editdialog/editdialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatatableComponent,
+    EditdialogComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatButtonModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTooltipModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
