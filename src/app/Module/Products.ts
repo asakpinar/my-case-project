b4b0b901d0ebd01b87a40062ec9264779da48399
Ahmet Şahin Akpınar <ahmetsahinakpnarr@gmail.com>
2022-11-14 22:17:23 +0300

    Module

---
+++ src/app/Module/Products.ts
@@ -0,0 +1,20 @@
+import { DatatableComponent } from "../components/datatable/datatable.component";
+
+export class Products{
+    id!: number;
+    title?: string;
+    description?: string;
+    price?:number;
+    rating?:number;
+    stock?:number;
+    brand?:string;
+    category?:string;
+    thumbnail?:string;
+    
+    }
+    
+    
+    
+    
+    
+    
